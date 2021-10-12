6ac478d053053c5af12572d127e3d768ed707b28
Jovanni Luna <jluna@foursquare.com>
2021-10-12 14:57:41 -0400

    models article and comment updated with new actions

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ApplicationRecord
     belongs_to :article
+
+    def article_should_be_published
+        errors.add(:article_id, 'is not published yet') if article && !article.published?
+    end
 end
