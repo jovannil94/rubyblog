49b58ab4bf6d30f98852280cfeea65ec38dc0bb6
Jovanni Luna <jluna@foursquare.com>
2021-10-12 13:51:55 -0400

    adds scropes to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,9 @@ class Article < ApplicationRecord
     has_and_belongs_to_many :categories
     has_many :comments
 
+    scope :published, -> { where.not(published_at: nil) }
+    scope :draft, -> { where(published_at: nil) }
+
     def long_title
         "#{title} - #{published_at}"
     end
