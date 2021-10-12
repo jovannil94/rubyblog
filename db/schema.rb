d45da5848a43f49257f6caa75caf5812ce2644b1
Jovanni Luna <jluna@foursquare.com>
2021-10-12 13:46:28 -0400

    creates model for comments, migrates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_173431) do
+ActiveRecord::Schema.define(version: 2021_10_12_174446) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2021_10_12_173431) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "article_id"
+    t.string "name"
+    t.string "email"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
