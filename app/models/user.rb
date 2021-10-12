2293e9a6bbcbe914c622018ef496874ba61a8821
Jovanni Luna <jluna@foursquare.com>
2021-10-12 14:52:15 -0400

    adds validations for email and password in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     validates :email, uniqueness: true
     validates :email, length: { in: 5..50 }
+    validates :email, format: { with: /\A[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}\z/i }
+    validates :password, confirmation: true
 
     has_one :profile
     has_many :articles, -> { order 'published_at DESC, title ASC' }, dependent: :nullify
